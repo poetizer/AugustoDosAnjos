56050eca4f01a75b72022400d3a8b4eb6040e213
Felipe Marinho <felipevm97@gmail.com>
2019-05-18 09:51:56 -0300

    esboço de poemas de Augusto dos Anjos

---
+++ poema.py
@@ -0,0 +1,30 @@
+from textgenrnn import textgenrnn
+import jamspell
+
+weigths = 'augusto-dos-anjos.hdf5'
+
+textgen = textgenrnn(weigths)
+
+# Descomente para treinar um novo modelo, ou melhorar.
+#
+# textgen.train_from_file('input.txt', num_epochs=100)
+# textgen.save(weigths)
+
+
+def gen(temp):
+    return list(map(str.strip, textgen.generate(n=14, temperature=temp, return_as_list=True)))
+
+
+corrector = jamspell.TSpellCorrector()
+corrector.LoadLangModel(
+    'drummond-dos-anjos.bin')
+
+gen = gen(1.0)
+poema = []
+
+for verso in gen:
+    novo_verso = corrector.FixFragment(verso)
+
+    poema.append(novo_verso)
+
+print('\n'.join(poema))
